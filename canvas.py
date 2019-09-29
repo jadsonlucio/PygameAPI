3d629aba76637807045965c4b2d31ceeedd097d2
jadsonlucio <jadsonaluno@hotmail.com>
2019-09-29 18:04:45 -0300

    remove files from root folder

--- canvas.py
+++
@@ -1,9 +0,0 @@
-from pygame import Surface
-
-class Canvas(Surface):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        super().__init__((width, height))
-
-        
